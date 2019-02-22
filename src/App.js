ca5c7d5de5975bd3d3ef5ad861cc915050fc2895
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2019-02-22 17:00:09 +0000

    Updated readme file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  Row
+} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Bye Saj!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#availabilityChecker">
+                Availability Checker
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container className="navbar-body-padding">
+          <Card>
+            <Card.Header>Check Availability</Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Row>
+                  <Col>
+                    <Form.Control placeholder="Enter phone number" />
+                  </Col>
+                  <Col>
+                    <Form.Control placeholder="Enter postcode" />
+                  </Col>
+                  <Col>
+                    <Button variant="primary" type="submit" block>
+                      Check
+                    </Button>
+                  </Col>
+                </Form.Row>
+              </Form>
+            </Card.Body>
+          </Card>
+          <Row>
+            <Col>&nbsp;</Col>
+            <Col>&nbsp;</Col>
+          </Row>
+          <Card>
+            <Card.Header>Select Address</Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group>
+                  <Form.Control as="select" multiple>
+                    <option>
+                      1 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      3 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      5 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      7 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      9 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      11 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                    <option>
+                      13 High Street, Rochdale, Lancashire, OL11 5ED
+                    </option>
+                  </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit" block>
+                  Select
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Container>
       </div>
     );
   }
