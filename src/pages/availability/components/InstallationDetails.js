769e4db786058fedc30295fcb75fbd2cea466193
Fahad Sarwar (Admin) <fsarwar.admin@office.zen.co.uk>
2019-03-16 19:47:06 +0000

    added form validation

--- src/pages/availability/components/InstallationDetails.js
+++ src/pages/availability/components/InstallationDetails.js
@@ -2,11 +2,61 @@ import React, { Component } from 'react';
 import {
   Button,
   Col,
+  FormFeedback,
   Input,
   Row
 } from 'reactstrap';
 
 class InstallationDetails extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            phoneNumber: "",
+            postcode: "",
+            postcodeIsValid: true
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.isPostcodeValid = this.isPostcodeValid.bind(this);
+        this.handleValidation = this.handleValidation.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange (event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    isPostcodeValid() {
+        return !(this.state.postcode === null || this.state.postcode.trim() === '');
+    }
+
+    handleValidation() {
+        let formIsValid = true;
+
+        this.setState({postcodeIsValid: this.isPostcodeValid()});
+
+        if(this.state.postcodeIsValid) {
+            formIsValid = false;
+        }
+
+        return formIsValid;
+    }
+
+    onFormSubmit (event) {
+        event.preventDefault();
+        
+        if(this.handleValidation())
+        {
+            console.log("Form is valid!");
+            return;
+        }
+
+        console.log("Form is invalid!");
+    }
+
     render() {
         return (
             <div>
@@ -16,6 +66,8 @@ class InstallationDetails extends Component {
                             name="phoneNumber"
                             id="phoneNumber"
                             placeholder="Enter phone number"
+                            value={this.state.phoneNumber}
+                            onChange={(event) => this.onInputChange(event)}
                         />
                     </Col>
                     <Col>
@@ -23,10 +75,14 @@ class InstallationDetails extends Component {
                             name="postcode"
                             id="postcode"
                             placeholder="Enter postcode"
+                            value={this.state.postcode}
+                            onChange={(event) => this.onInputChange(event)}
+                            invalid={!this.state.postcodeIsValid}
                         />
+                        <FormFeedback valid={this.state.postcodeIsValid}>Please enter a postcode</FormFeedback>
                     </Col>
                     <Col>
-                        <Button block color="primary" type="submit">
+                        <Button block color="primary" type="submit" onClick={(event) => this.onFormSubmit(event)}>
                             Check
                         </Button>
                     </Col>
